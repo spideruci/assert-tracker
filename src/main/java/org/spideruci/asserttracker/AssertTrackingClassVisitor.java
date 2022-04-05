50807730bd07b975fd4bce1b7af488e2f89e9e99
standing_out_flamingo <72062309+qinfendeheichi@users.noreply.github.com>
2022-04-05 12:58:01 -0700

    pair assertions with Test Class and Test Method
    Recognize a Test Class whose classname ends with "Test"
    Recognize an outer Test Method with the annotation "@Test"
    Instrument the code the indicate the test class and test method for each assertion statement

--- src/main/java/org/spideruci/asserttracker/AssertTrackingClassVisitor.java
+++ src/main/java/org/spideruci/asserttracker/AssertTrackingClassVisitor.java
@@ -6,23 +6,43 @@ import org.objectweb.asm.MethodVisitor;
 
 public class AssertTrackingClassVisitor extends ClassVisitor {
 
+    public boolean isTestClass;
+    public String testClassName;
     public AssertTrackingClassVisitor(int api) {
         super(api);
+        this.isTestClass=false;
+        this.testClassName = "unknown";
     }
 
     public AssertTrackingClassVisitor(int api, ClassWriter cw) {
         super(api, cw);
+        this.isTestClass=false;
+        this.testClassName = "unknown";
+
     }
+
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        if (name.endsWith("Test")) {
+            this.isTestClass = true;
+            this.testClassName = name.replace("/",".");
+        }else{
+            this.testClassName = "NoneTestClass";
+        }
+        super.visit(version, access, name, signature, superName, interfaces);
+    }
+
 
     @Override
     public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
         
         if (this.cv != null) {
             MethodVisitor methodWriter = this.cv.visitMethod(access, name, descriptor, signature, exceptions);
-            return new AssertVisitor(this.api, name, methodWriter);
+            return new AssertVisitor(this.api, name, methodWriter, this.isTestClass, this.testClassName);
         }
         else {
-            return new AssertVisitor(this.api, name);
+            return new AssertVisitor(this.api, name,this.isTestClass, this.testClassName);
         }
     }
     
