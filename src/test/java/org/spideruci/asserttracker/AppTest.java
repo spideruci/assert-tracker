5547a5cebfe5446d80173f3aa6eab3035af72ac1
standing_out_flamingo <72062309+qinfendeheichi@users.noreply.github.com>
2022-05-03 19:45:39 -0700

    verify printing local variables with XStream

--- src/test/java/org/spideruci/asserttracker/AppTest.java
+++ src/test/java/org/spideruci/asserttracker/AppTest.java
@@ -1,11 +1,13 @@
 package org.spideruci.asserttracker;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
+ * run mvn test 1> out.txt 2> result.txt
+ * instrumentation error printing results are in result.txt file.
  */
 public class AppTest 
 {
@@ -25,6 +27,59 @@ public class AppTest
     }
 
     @Test
+    public void shouldDumpInt(){
+        int i =10;
+        assertEquals(10,i);
+    }
+
+    @Test
+    public void shouldDumpByteCharShort(){
+        byte b = 'd';
+        assertEquals('d',b);
+        char c = 's';
+        assertEquals('s',c);
+        short s = 1;
+        assertEquals(1, s);
+    }
+
+    @Test
+    public void shouldDumpChar(){
+
+    }
+
+    @Test
+    public void shouldDumpLong(){
+        long k = 3;
+        assertEquals(3, k);
+    }
+
+    @Test
+    public void shouldDumpFloat(){
+        float k = 3;
+        assertEquals(3, k,0.00001);
+    }
+
+    @Test
+    public void shouldDumpDouble(){
+        double k = 3;
+        assertEquals(3, k,0.0001);
+    }
+
+    @Test
+    public void shouldDumpObject(){
+        Person p = new Person(19, true);
+        assertNotEquals(new Person(19,true), p);
+    }
+
+    @Test
+    public void shouldDumpArray(){
+        Person[] persons =new Person[2];
+        persons[0]=new Person(13,false);
+        persons[1]= new Person(10,true);
+        assertTrue(persons[1].sex);
+    }
+
+    @Test
     public void shouldPassAndFailWithLocalVariables()
     {
 
@@ -41,6 +96,7 @@ public class AppTest
         }
 
         if (i == 0) {
+            float a = 3;
             assertTrue(true);
             int j2 = 9;
             assertTrue(true);
@@ -53,5 +109,14 @@ public class AppTest
         System.out.println(k);
 
         assertTrue( false );
+    }
+}
+
+class Person{
+    public int age;
+    public Boolean sex;
+    public Person(int age, Boolean sex){
+        this.age = age;
+        this.sex = sex;
     }
 }
