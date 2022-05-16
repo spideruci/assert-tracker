8e2aac7e7d6deecf40d60f312a72b96950b6202d
standing_out_flamingo <72062309+qinfendeheichi@users.noreply.github.com>
2022-05-16 13:29:50 -0700

    Update zipFileManger
    number of digits of System.nanoTime() is not certain

--- src/main/java/ZipFileManager.java
+++ src/main/java/ZipFileManager.java
@@ -1,11 +1,9 @@
-
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.*;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -32,42 +30,59 @@ public class ZipFileManager {
             zipPath ="jar:file:"+currentPath.substring(matcher.end()).replace("\\","/")
                     +"/"+"xmlOutput.zip";
         }
-        deleteFile("xmlOutput.zip");
+//        deleteFile("xmloutput.zip");
         URI zip_loc = URI.create(zipPath);
-
         try (FileSystem zipfs = FileSystems.newFileSystem(zip_loc, zip_properties)) {
-            while(true){
-                File file = new File("xmlOutput");
-                List<Path> xmlFiles = null;
-                try (Stream<Path> fileStream = Files.walk(file.toPath())) {
-                    xmlFiles = fileStream.filter(Files::isRegularFile)
-                            .filter(p -> p.toFile().getName().endsWith(".xml"))
-                            .collect(Collectors.toList());
-                }
-
-                for (Path xmlFile : xmlFiles) {
-                    Path filePathInZip =zipfs.getPath(xmlFile.toString().substring(9).replace("\\"," "));//.replace("\\","/"));
-                    Files.copy(xmlFile,filePathInZip);
-                }
-                Thread.sleep(3000);
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        zip_properties.put("create","false");
+        int count= 0;
+        while(true){
+            File file = new File("xmloutput");
+            List<Path> xmlFiles = null;
+            try (Stream<Path> fileStream = Files.walk(file.toPath())) {
+                xmlFiles = fileStream.filter(Files::isRegularFile)
+                        .filter(p -> p.toFile().getName().endsWith(".xml"))
+                        .collect(Collectors.toList());
+            }
 
-//        URI zip_loc = URI.create("jar:file:/"+ "Users/勤奋的黑痴/OneDrive/Java/test/niubi.zip");
-
-
-    }
-    public static void deleteFile(String path) throws Exception {
-        File aFile = new File(path);
-        if(aFile.exists()){
-            aFile.delete();
-        }else{
-            throw new Exception(path+ " file not exists");
+            List<Path> deleteFiles = new LinkedList<Path>();
+            try (FileSystem zipfs = FileSystems.newFileSystem(zip_loc, zip_properties)) {
+                for (Path xmlFile : xmlFiles) {
+                    File nextFile = new File(xmlFile.toString());
+                    long currentTime = System.nanoTime();
+                    int size = xmlFile.toString().length();
+                    int length = xmlFile.toString().split(" ")[1].length()-4;
+                    long time = new Long(xmlFile.toString().substring(size-4-length,size-4));
+                    //move the file 30 seconds after it was created
+                    if ((currentTime-time)/1000000000>30 && nextFile.canRead() && nextFile.canWrite()){
+//                        System.out.println(xmlFile);
+                        deleteFiles.add(xmlFile);
+                        Path filePathInZip = zipfs.getPath(xmlFile.toString().substring(9).replace("\\", " "));//.replace("\\","/"));
+                        Files.copy(xmlFile, filePathInZip,StandardCopyOption.REPLACE_EXISTING);//
+                        count = count+1;
+                        if(count%1000==0){
+                            System.out.println("moved "+count+" files");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for(Path p: deleteFiles){
+                deleteFile(p.toString());
+            }
+            Thread.sleep(1000);
         }
-
+    }
+    public static void deleteFile(String path) {
+        File aFile = new File(path);
+        int count = 0;
+        if (aFile.exists()) {
+            aFile.delete();
+        }
     }
 
 }
